77c951203bc12505ec2776eec6cec6f2f527328b
Crew Spence <csspence123@gmail.com>
2020-05-25 06:57:48 -0500

    Created function stub for Udemy toy problem.

---
+++ frequency_counter_udemy.js
@@ -0,0 +1,15 @@
+/*
+This is a problem from the Udemy course on algorithms (https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/9816152#overview).
+In this particular example problem, I'm supposed to use a Frequency Counter.
+
+Write a function called same, which accepts two arrays.  The function should return true if every value in the array has its corresponding
+value squared in the second array.  The frequency of values must be the same.
+
+same([1, 2, 3], [4, 1, 9]) // true
+same([1, 2, 3], [1, 9]) // false
+same([1, 2, 1], [4, 4, 1]) // false (must be the same frequency)
+*/
+
+const same = (arr1, arr2) => {
+  
+}
